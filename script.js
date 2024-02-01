3bed7162daf3fcacdc2c209e5ceb3b956bc5f099
xgsarah <sjbquimpo@gmail.com>
2024-02-02 07:20:31 +0800

    implement displaying of scores and rounds

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-let round = 0;
+let round = 1;
 const selection = ['rock', 'paper', 'scissors'];
 const score = [];
 
-function getOverAllWinner() {
+function getScores() {
   const computer = score.filter((s) => s === 'computer');
   const player = score.filter((s) => s === 'player');
 
-  console.warn(
-    `====================${
-      player.length > computer.length ? 'Congratulations!!' : 'Not Bad'
-    }====================`
-  );
-  console.info(`Overall Scores:`);
-  console.table({ computer: computer.length, player: player.length });
+  return { computer: computer.length, player: player.length };
 }
 
 function getWinner(playerSelection, computerSelection) {
@@ -44,24 +38,12 @@ function getWinner(playerSelection, computerSelection) {
 }
 
 function playRound(playerSelection, computerSelection) {
-  if (!selection.find((s) => s === playerSelection)) {
-    return;
-  }
-
-  console.warn(`================ROUND ${round + 1}===================`);
-  console.log("Computer's pick: ", computerSelection);
-  console.log('Your pick: ', playerSelection);
-
   const winner = getWinner(playerSelection, computerSelection);
   score.push(winner);
 
-  if (winner === 'player') {
-    console.log(`You won! ${playerSelection} beats ${computerSelection}`);
-  } else if (winner === 'computer') {
-    console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-  } else {
-    console.log("It's a tie");
-  }
+  displayScore();
+  displayWinner(playerSelection, computerSelection);
+  displayRound();
   round++;
 }
 
@@ -70,22 +52,71 @@ function getComputerChoice() {
   return selection[index];
 }
 
-function getUserChoice() {
-  return prompt("pick from 'rock', 'paper' or 'scissors'");
-}
+// function getUserChoice() {
+//   return prompt("pick from 'rock', 'paper' or 'scissors'");
+// }
 
-function playGame() {
-  while (round < 6) {
-    if (round === 5) {
-      getOverAllWinner();
-      console.log('Please reload to play again.');
-      round++;
-    } else {
-      const user = getUserChoice();
-      const computer = getComputerChoice();
-      playRound(user, computer);
-    }
+// function playGame() {
+//   while (round < 6) {
+//     if (round === 5) {
+//       getOverAllWinner();
+//       console.log('Please reload to play again.');
+//       round++;
+//     } else {
+//       const user = getUserChoice();
+//       const computer = getComputerChoice();
+//       playRound(user, computer);
+//     }
+//   }
+// }
+
+const rockButton = document.querySelector('#rock');
+rockButton.addEventListener('click', () => {
+  playRound('rock', getComputerChoice());
+});
+
+const paperButton = document.querySelector('#paper');
+paperButton.addEventListener('click', () => {
+  playRound('paper', getComputerChoice());
+});
+
+const scissorsButton = document.querySelector('#scissors');
+scissorsButton.addEventListener('click', () => {
+  playRound('scissors', getComputerChoice());
+});
+
+const playerScore = document.querySelector('#player-score');
+const computerScore = document.querySelector('#computer-score');
+
+const displayScore = () => {
+  const scores = getScores();
+
+  playerScore.textContent = scores.player;
+  computerScore.textContent = scores.computer;
+  console.log(scores);
+};
+
+const displayWinner = (playerSelection, computerSelection) => {
+  const div = document.querySelector('.weapons');
+  const messageContainer = div.querySelector('h2');
+
+  const winner = getWinner(playerSelection, computerSelection);
+  let message = '';
+
+  if (winner === 'player') {
+    message = `You won! ${playerSelection} beats ${computerSelection}`;
+  } else if (winner === 'computer') {
+    message = `You lose! ${computerSelection} beats ${playerSelection}`;
+  } else {
+    message = "It's a tie";
   }
-}
+
+  messageContainer.textContent = message;
+};
+
+const displayRound = () => {
+  const div = document.querySelector('#game-round');
+  div.textContent = round;
+};
 
 // playGame();
